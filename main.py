c1a7cf65c3ec5f0a7b044f9ba3e2ec8413da53bb
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2021-11-01 20:53:06 -0500

    test commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tika import parser
 
 input_pdf = r'C:\Users\alanv\PythonCode\Projects\Inspired\Packing_List\96759.pdf'
 parsed_pdf = parser.from_file(input_pdf)
-# print(parsed_pdf)
+print(parsed_pdf)
 print(parsed_pdf['content'])
 
 
